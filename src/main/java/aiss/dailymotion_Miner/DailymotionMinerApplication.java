21fe9104cb2d3dd0638cba882dfbc2b903de0094
Lorena PardáLara <lorenaparlar@gmail.com>
2026-05-05 18:20:51 +0200

    clases y carpetas

--- src/main/java/aiss/dailymotion_Miner/DailymotionMinerApplication.java
+++ src/main/java/aiss/dailymotion_Miner/DailymotionMinerApplication.java
@@ -2,6 +2,9 @@ package aiss.dailymotion_Miner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 
 @SpringBootApplication
 public class DailymotionMinerApplication {
@@ -10,4 +13,6 @@ public class DailymotionMinerApplication {
 		SpringApplication.run(DailymotionMinerApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {return builder.build();}
 }
