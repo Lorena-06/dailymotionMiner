de054b07e6cee327f282acf7a1d7339067eeca16
Lorena PardáLara <lorenaparlar@gmail.com>
2026-05-06 18:41:32 +0200

    subtitles

---
+++ src/main/java/aiss/dailymotion_Miner/model/dailymotion/Caption.java
@@ -0,0 +1,63 @@
+
+package aiss.dailymotion_Miner.model.dailymotion;
+
+import javax.annotation.processing.Generated;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "id",
+    "language"
+})
+@Generated("jsonschema2pojo")
+public class Caption {
+
+    @JsonProperty("id")
+    private String id;
+    @JsonProperty("language")
+    private String language;
+
+    @JsonProperty("id")
+    public String getId() {
+        return id;
+    }
+
+    @JsonProperty("id")
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @JsonProperty("language")
+    public String getLanguage() {
+        return language;
+    }
+
+    @JsonProperty("language")
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(Caption.class.getName()).append('@').append(Integer.toHexString(System.identityHashCode(this))).append('[');
+        sb.append("id");
+        sb.append('=');
+        sb.append(((this.id == null)?"<null>":this.id));
+        sb.append(',');
+        sb.append("language");
+        sb.append('=');
+        sb.append(((this.language == null)?"<null>":this.language));
+        sb.append(',');
+        if (sb.charAt((sb.length()- 1)) == ',') {
+            sb.setCharAt((sb.length()- 1), ']');
+        } else {
+            sb.append(']');
+        }
+        return sb.toString();
+    }
+
+}
