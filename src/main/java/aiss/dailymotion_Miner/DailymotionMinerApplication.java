d5909e8188a8e71a2f0141e589cd4013536a4cd4
Lorena PardáLara <lorenaparlar@gmail.com>
2026-05-07 16:40:18 +0200

    .

--- src/main/java/aiss/dailymotion_Miner/DailymotionMinerApplication.java
+++ src/main/java/aiss/dailymotion_Miner/DailymotionMinerApplication.java
@@ -2,19 +2,12 @@ package aiss.dailymotion_Miner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class DailymotionMinerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DailymotionMinerApplication.class, args);
-	}
-
-	@Bean
-	public RestTemplate restTemplate(){
-		return new RestTemplate();
 	}
 }
 
